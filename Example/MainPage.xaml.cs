45e4c7a312022f11ada86cbfa1599bd6fe2eccc5
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2024-02-26 21:50:53 -0300

    feat: Adicionado botão de saudação

---
+++ Example/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Example
+{
+    public partial class MainPage : ContentPage
+    {
+        //Declaração de variável: tipo nomeDaVariavel = 0;
+        int count = 0;
+        string nome = "";
+ 
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void OnNameClicked(object sender, EventArgs e)
+        {
+            nome = NameEntry.Text;
+            //Interpolação
+            GreetingLabel.Text = $"Hello, {nome}";
+
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+
+
+
+    }
+
+}
